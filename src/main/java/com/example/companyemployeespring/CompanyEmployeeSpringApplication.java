993b2f3590def369e61cebf33d956159b9b83059
sevakmartirosyan92 <sevakmarti@gmail.com>
2022-09-27 17:24:41 +0400

    CompanyEmployeeSpringApplication

--- src/main/java/com/example/companyemployeespring/CompanyEmployeeSpringApplication.java
+++ src/main/java/com/example/companyemployeespring/CompanyEmployeeSpringApplication.java
@@ -1,13 +1,9 @@
 package com.example.companyemployeespring;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class CompanyEmployeeSpringApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CompanyEmployeeSpringApplication.class, args);
     }
-
 }
